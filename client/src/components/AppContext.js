22d73ec7c183f682236b7146646da279f74c6b38
donnyesq <adonis@darmas.ca>
2020-10-05 19:52:44 -0400

    exercise 2 complete

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,6 +1,9 @@
 import React, { createContext, useEffect, useState } from "react";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase";
+import "firebase/auth";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyAqGfJuK_7V797gwLf1Emd9K-BzKYr6XaI",
   authDomain: "user-app-44e10.firebaseapp.com",
   databaseURL: "https://user-app-44e10.firebaseio.com",
@@ -10,10 +13,40 @@ const firebaseConfig = {
   appId: "1:24161197699:web:be60090867e10b9a5985b6",
 };
 
-export const AppContext = createContext(null);
-
-const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
 };
 
-export default AppProvider;
+export const AppContext = createContext(null);
+
+const AppProvider = ({ children, signInWithGoogle, signOut, user }) => {
+  const [appUser, setAppUser] = React.useState({});
+
+  const handleSignOut = () => {
+    signOut();
+    setAppUser({});
+  };
+
+  React.useEffect(() => {
+    if (user) {
+      setAppUser({
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+      });
+    }
+  }, [user]);
+
+  return (
+    <AppContext.Provider value={{ appUser, signInWithGoogle, handleSignOut }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(AppProvider);
