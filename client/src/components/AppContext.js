f10222c697130ae1246137afc672332a77172cc9
donnyesq <adonis@darmas.ca>
2020-10-05 19:00:34 -0400

    exercise 1 complete

--- client/src/components/AppContext.js
+++ client/src/components/AppContext.js
@@ -1,4 +1,14 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqGfJuK_7V797gwLf1Emd9K-BzKYr6XaI",
+  authDomain: "user-app-44e10.firebaseapp.com",
+  databaseURL: "https://user-app-44e10.firebaseio.com",
+  projectId: "user-app-44e10",
+  storageBucket: "user-app-44e10.appspot.com",
+  messagingSenderId: "24161197699",
+  appId: "1:24161197699:web:be60090867e10b9a5985b6",
+};
 
 export const AppContext = createContext(null);
 
